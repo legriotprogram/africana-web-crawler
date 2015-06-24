4957c8578d4d7b28b97d8d92a923e54d6ff24524
legriot13 <codeu242@gmail.com>
2015-06-23 20:46:07 -0400

    Completed basic web crawler
    This code converts all the source code of a given web page, excluding empty break lines, into text which is stored in a large array.

--- crawler.py
+++ crawler.py
@@ -1,13 +1,18 @@
 import HTMLParser, urllib
 
-urlText=[]
+urlText=[]		# the collection of all the text from a given page
 
-class parseText(HTMLParser.HTMLParser):
-	def handle_data(self, data):
-		if data!= '\n'
+class parseText(HTMLParser.HTMLParser): # a class that extends the python HTMLParser
+	def handle_data(self, data):		# this parser excludes empty line breaks
+		if data != '\n':
 			urlText.append(data)
-			
+
+lParser = parseText()	# initializes a text parser
 weblink = raw_input("Please enter the URL you wish to crawl:") # asks the user for a url 
 print "Currently opening", weblink	# prints out the url entered -- for testing purposes (FTPs)
-handle = urllib.urlopen(weblink)	# creates a variable handle which points to the url of interest
-html_gunk = handle.read() 		# sets a string 'html_gunk' equal to the html content read from the page
+site_pointer = urllib.urlopen(weblink) # creates a variable 'site_pointer' which points to the url of interest
+raw_html = site_pointer.read() 		# sets a string 'raw_html' equal to the html content read from the page
+lParser.feed(raw_html)				#feeds the raw html to our special html parser
+lParser.close()			# closes the text parser
+for item in urlText:	# prints out all the text read from the given page -- FTPs
+	print item
